984510001fae82339bff1aa1d42144b9712ed386
Lam Nguyen <lamnguyen3801@gmail.com>
2020-03-23 13:39:53 -0700

    upload lab#3-Stock Event

---
+++ Lab-Assignment-3/Stock.cs
@@ -0,0 +1,77 @@
+﻿// Lam Nguyen
+// Lab Assignment 3: Problem 2
+// Event Publisher
+using System;
+using System.Threading;
+
+namespace stockEvent {
+    public class Stock {
+        // Create event object
+        public event EventHandler<StockNotification> stockEvent;
+
+        private String _name;
+        private int _intialValue;
+        private int _maxChange;
+        private int _notificationThreshold;
+        private int _currentValue;
+        private int _numberChanges;
+        private DateTime _dateTime;
+
+        // Stock Constructor: assign values for all data members
+        public Stock(String name, int startingValue, int maxChange, int threshold) {
+            _name = name;
+            _intialValue = startingValue;
+            _currentValue = _intialValue;
+            _maxChange = maxChange;
+            _notificationThreshold = threshold;
+            _dateTime = DateTime.Now;
+
+
+            //Create a thread
+            Thread thread = new Thread(Activate);
+            thread.Start();
+        }
+
+        // Method to activate the thread and call ChangeStockValue method
+        public void Activate() {
+            for(int i = 0; i < 15; i++) {
+                Thread.Sleep(500);
+                ChangeStockValue();
+            }
+        }
+
+        // Invokes the stockEvent (of event-type StockNotification)
+        public void ChangeStockValue() {
+            Random rand = new Random();
+            int num = rand.Next(0, _maxChange);
+            _currentValue += num;
+            _numberChanges++;
+
+            if((Math.Abs(_currentValue - _intialValue))> _notificationThreshold) {
+                stockEvent?.Invoke(this, new StockNotification(_name, _currentValue, _numberChanges, _dateTime));
+            }
+
+        }
+
+    }
+
+    public class StockNotification : EventArgs {
+        // Stores information for the event
+        public StockNotification(String stockName, int currentValue, int numberChanges, DateTime dateTime) {
+            StockName = stockName;
+            CurrentValue = currentValue;
+            NumberChanges = numberChanges;
+            DateAndTime = dateTime;
+        }
+        // Get stockName
+        public String StockName { get; }
+        // Get currentValue
+        public int CurrentValue { get; }
+        // Get numberChanges
+        public int NumberChanges { get; }
+        // Get dateAndTime
+        public DateTime DateAndTime { get;  }
+    }
+
+
+}
---
+++ Lab-Assignment-3/StockBroker.cs
@@ -0,0 +1,62 @@
+﻿// Lam Nguyen
+// Lab Assignment 3: Problem 2
+// Event subscriber
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace stockEvent {
+    public class StockBroker {
+        private String _brokerName;
+        protected List<Stock> stocks;
+        private static object locker = new Object();
+        /// <summary>
+        /// StockBroker constructor
+        /// </summary>
+        /// <param name="name"> name of the stock broker </param>
+        public StockBroker(String name) {
+            stocks = new List<Stock>();
+            _brokerName = name;
+        }
+
+        /// <summary>
+        /// registers the Notify listerner with the stock
+        /// (in addition to adding it to the list of stocks held by the broker)
+        /// </summary>
+        /// <param name="stock"> stock object </param>
+        public void AddStock(Stock stock) {
+            stocks.Add(stock);
+            stock.stockEvent += EventHandler;
+            stock.stockEvent += WriteToFile;
+            
+        }
+
+        /// <summary>
+        /// Outputs to the console the name, value, and the number of changes of the stock
+        /// whose value is out of the range given the stock's notification threshold
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void EventHandler(object sender, StockNotification e) {
+            Console.WriteLine($"{_brokerName.PadRight(10)} {e.StockName.ToString().PadRight(10)} {e.CurrentValue.ToString().PadRight(10)} {e.NumberChanges.ToString().PadRight(10)} {e.DateAndTime.ToString()}");
+        }
+
+        // Print the output to a text file
+        public void WriteToFile(object sender, StockNotification e) {
+            try {
+                //Wait for the resource to be free
+                lock (locker) {
+                    // Set a variable to the Documents path.
+                    string docPath = "/Users/lamnguyen/Desktop/CSULB/Semesters/Spring-2020/CECS-475/Assignments/A-3/A(3)-Part2";
+
+                    using (FileStream file = new FileStream(Path.Combine(docPath, "output.txt"), FileMode.Append, FileAccess.Write, FileShare.Read))
+                    using (StreamWriter writer = new StreamWriter(file)) {
+                        writer.WriteLine(_brokerName.PadRight(10) + e.StockName.PadRight(20) + e.CurrentValue.ToString().PadRight(15) + e.NumberChanges.ToString().PadRight(10) + e.DateAndTime.ToString());
+                    }
+                }
+            }
+            catch (IOException) { }
+        }
+    }
+
+}
